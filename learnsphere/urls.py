dcf5085c744fd011f3a8f9d1ab76e6691f1dfd37
Taylan Ozveren <taylantr.respect@gmail.com>
2025-05-27 00:17:07 +0300

    feat: add /healthz endpoint for Render health checks

--- learnsphere/urls.py
+++ learnsphere/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.urls import path
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -24,6 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("accounts/", include("allauth.urls")),  # <-- BURASI!
+    path("healthz", lambda request: HttpResponse("OK")),
 ]
 
 if settings.DEBUG:
